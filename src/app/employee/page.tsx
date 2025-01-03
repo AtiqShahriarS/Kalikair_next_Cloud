439a12b0c4b7a52aeda630ce4341b626c7440bc0
AtiqShahriarS <atiq.shahriar@gmail.com>
2025-01-03 03:33:16 -0800

    delete

--- src/app/employee/page.tsx
+++
@@ -1,8 +0,0 @@
-// app/employees/page.tsx
-
-import EmployeeList from '@/components/EmployeeList';
-import React from 'react';
-
-export default function employee() {
-  return <EmployeeList />;
-}
--- src/app/gallery/page.tsx
+++
@@ -1,6 +0,0 @@
-//gallery/page.tsx
-import GalleryPage from '@/components/gallery_stuff/GalleryPage';
-
-export default function Gallery() {
-  return <GalleryPage />;
-}
